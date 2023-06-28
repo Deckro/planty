489c66d44dd045cd4fe8fd97baa994bd6cb05128
deckro <deck_soufly@hotmail.fr>
2023-06-28 15:13:37 +0200

    changement page accueil

--- wp-content/themes/planty/index.php
+++ wp-content/themes/planty/index.php
@@ -1,44 +1,58 @@
 <?php require 'header.php' ?>
 <main>
 	<article class="presentation">
-		<h1>BOISSON ENERGISANTE 100% NATURELLE</h1>
+		<h1 class="titre1" >BOISSON ENERGISANTE 100% NATURELLE</h1>
 		<div class="energie">
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Group 4.png";?>" alt="gauche">
+			<img class="coté" src="<?php echo get_stylesheet_directory_uri()."/img/Group 4.png";?>" alt="gauche">
 			<img src="<?php echo get_stylesheet_directory_uri()."/img/Planty6 1.png";?>" alt="centre">
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Group 3.png";?>" alt="droite">
+			<img class="coté" src="<?php echo get_stylesheet_directory_uri()."/img/Group 3.png";?>" alt="droite">
 		</div>
 	</article>
 	<article class="plante">
-		<h2>L'énergie des plante</h2>
-		<p>Planty, c’est la première boisson énergisante composée à 100 % de produits naturels. Avec son goût frais et fruité, retrouvez votre énergie grâce aux plantes riches en vitamine B2, à n’importe quel moment de la journée.</p>
+		<h2 class="titre2">L'énergie des plante</h2>
+		<div class="info">
+			<p>Planty, c’est la première boisson énergisante composée à 100 % de produits naturels. Avec son goût frais et fruité, retrouvez votre énergie grâce aux plantes riches en vitamine B2, à n’importe quel moment de la journée.</p>
+		</div>
 	</article>
 	<article class="gout">
-		<h2>LES Goûts</h2>
+		<h2 class="titre3">LES Goûts</h2>
 		<p>Nous avons une saveur pour chaque moment de votre journée.</p>
-		<div>
+		<div class="ligne">
 			<img src="<?php echo get_stylesheet_directory_uri()."/img/jeremy-bezanger-0QgbyZdhX7k-unsplash 1.png";?>" alt="fraise">
 			<img src="<?php echo get_stylesheet_directory_uri()."/img/estudio-bloom-tOitjphtIXU-unsplash 1.png";?>"  alt="pamplemousse">
+		</div>
+		<div class="ligne">
 			<img src="<?php echo get_stylesheet_directory_uri()."/img/rodion-kutsaev-4k8xEFW4_3Q-unsplash 1.png";?>"  alt="framboise">
 			<img src="<?php echo get_stylesheet_directory_uri()."/img/estudio-bloom-ezqnxsqUZ80-unsplash 1.png";?>"  alt="citron">
 		</div>
 	</article>
-	<article class="parfum">
-		<h2>CE QU’ILS EN PENSENT</h2>
-		<div>
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-3.png";?>" alt="gauche">
-			<h3>Fatiha</h3>
-			<p>La meilleure boisson énergisante disponible sur le marché ! </p>
+	<article class="avis">
+		<h2 class="titre4">CE QU’ILS EN PENSENT</h2>
+		<div class="ce">
+			<div class="card">
+				<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-3.png";?>" alt="gauche">
+				<div class="card1">
+					<h3>Fatiha</h3>
+					<p>La meilleure boisson énergisante disponible sur le marché ! </p>
+				</div>
+			</div>
+			<div class="card">
+				<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-4.png";?>" alt="centre">
+				<div class="card1">
+					<h3>Véro</h3>
+					<p>Frais & fruité c’est parfait pour accompagner le goûter.</p>
+				</div>
+			</div>
+			<div class="card">
+				<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-5.png";?>" alt="milieu">
+				<div class="card1">
+					<h3>Marc</h3>
+					<p>Un boost d’énergie sans produits chimiques, un vrai plus dans ma journée.</p>
+				</div>
+			</div>
 		</div>
-		<div>
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-4.png";?>" alt="centre">
-			<h3>Véro</h3>
-			<p>Frais & fruité c’est parfait pour accompagner le goûter.</p>
-		</div>
-		<div>
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-5.png";?>" alt="milieu">
-			<h3>Marc</h3>
-			<p>Un boost d’énergie sans produits chimiques, un vrai plus dans ma journée.</p>
-		</div>
+	</article>
+	<article class="frise">
 		<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group(1).png";?>" alt="tout le bas">
 	</article>
 </main>
