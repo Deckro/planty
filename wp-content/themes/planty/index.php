77dfeda45ae116a5974dd7a0b0b0cc0414604766
deckro <deck_soufly@hotmail.fr>
2023-06-28 10:55:34 +0200

    mise en place des img

--- wp-content/themes/planty/index.php
+++ wp-content/themes/planty/index.php
@@ -16,30 +16,30 @@
 		<h2>LES Goûts</h2>
 		<p>Nous avons une saveur pour chaque moment de votre journée.</p>
 		<div>
-			<img src="img/jeremy-bezanger-0QgbyZdhX7k-unsplash 1.png" alt="fraise">
-			<img src="img/estudio-bloom-tOitjphtIXU-unsplash 1.png" alt="pamplemousse">
-			<img src="img/rodion-kutsaev-4k8xEFW4_3Q-unsplash 1.png" alt="framboise">
-			<img src="img/estudio-bloom-ezqnxsqUZ80-unsplash 1.png" alt="citron">
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/jeremy-bezanger-0QgbyZdhX7k-unsplash 1.png";?>" alt="fraise">
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/estudio-bloom-tOitjphtIXU-unsplash 1.png";?>"  alt="pamplemousse">
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/rodion-kutsaev-4k8xEFW4_3Q-unsplash 1.png";?>"  alt="framboise">
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/estudio-bloom-ezqnxsqUZ80-unsplash 1.png";?>"  alt="citron">
 		</div>
 	</article>
 	<article class="parfum">
 		<h2>CE QU’ILS EN PENSENT</h2>
 		<div>
-			<img src="img/Mask group-3.png" alt="gauche">
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-3.png";?>" alt="gauche">
 			<h3>Fatiha</h3>
 			<p>La meilleure boisson énergisante disponible sur le marché ! </p>
 		</div>
 		<div>
-			<img src="img/Mask group-4.png" alt="centre">
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-4.png";?>" alt="centre">
 			<h3>Véro</h3>
 			<p>Frais & fruité c’est parfait pour accompagner le goûter.</p>
 		</div>
 		<div>
-			<img src="img/Mask group-5.png" alt="milieu">
+			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-5.png";?>" alt="milieu">
 			<h3>Marc</h3>
 			<p>Un boost d’énergie sans produits chimiques, un vrai plus dans ma journée.</p>
 		</div>
-		<img src="img/Mask group-1.png" alt="tout le bas">
+		<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group(1).png";?>" alt="tout le bas">
 	</article>
 </main>
 <?php require 'footer.php';?>
