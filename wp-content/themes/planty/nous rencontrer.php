653633e2e529d4fae6b27f8c675dcbee3f1b091b
deckro <deck_soufly@hotmail.fr>
2023-06-29 14:09:00 +0200

    mise en page nous rencontrer

--- wp-content/themes/planty/nous rencontrer.php
+++ wp-content/themes/planty/nous rencontrer.php
@@ -1,41 +1,54 @@
 <?php /* Template Name: nous rencontrer */ ?>
 <?php require "header.php"; ?>
-<main>
-    <article>
-    <h1>nous rencontrer</h1>
-    <p>Chez Planty nous sommes tous passionnés par le bien-être, et ça se retrouve dans nos boissons ! Notre start-up s’est construite au fur et à mesure de rencontres entre amoureux des plantes.</p>
-    </article>
-    <article>
-        <h2>L'équipe</h2>
-        <div>
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-2.png";?>" alt="gauche">
-			<div>
-				<h3>mégane</h3>
-				<p>CEO</p>
-			</div>
-		</div>
-        <div>
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-1.png";?>" alt="gauche">
-			<div>
-				<h3>brooke</h3>
-				<p>Nutritionniste</p>
-			</div>
-		</div>
-        <div>
-			<img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group.png";?>" alt="gauche">
-			<div>
-				<h3>sylvie</h3>
-				<p>Mixologue</p>
-			</div>
-            <img src="<?php echo get_stylesheet_directory_uri()."/img/Group 3.png";?>" alt="droit">
-		</div>
-    </article>
-    <article>
-        <h2>Nous contacter</h2>
-        <h4>Nom</h4><input type="text">
-        <h4>E-mail</h4><input type="email">
-        <h4>Message</h4><textarea aria-errormessage=""></textarea>
-    </article>
-    <img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group(1).png";?>" alt="bas">
-</main>
+
+    <main>
+        <article class="nous">
+            <img src="<?php echo get_stylesheet_directory_uri()."/img/Group 2.png";?>" alt="gauche">
+            <div>
+                <h1>nous rencontrer</h1>
+                <p class="textnous">Chez Planty nous sommes tous passionnés par le bien-être, et ça se retrouve dans nos boissons ! Notre start-up s’est construite au fur et à mesure de rencontres entre amoureux des plantes.</p>
+            </div>
+        </article>
+        <article>
+            <h2 class="equipe">L'équipe</h2>
+            <div  class="équipe">
+                <div>
+			        <img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-2.png";?>" alt="gauche">
+			        <div>
+				        <h3>mégane</h3>
+				        <p>CEO</p>
+			        </div>
+		        </div>
+                <div>
+			        <img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group-1.png";?>" alt="gauche">
+			        <div>
+				        <h3>brooke</h3>
+	    			    <p>Nutritionniste</p>
+		    	    </div>
+		        </div>
+                <div>
+	    		    <img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group.png";?>" alt="gauche">
+		    	    <div>
+			    	    <h3>sylvie</h3>
+				        <p>Mixologue</p>
+			        </div>
+                </div>
+                <img class="fleur" src="<?php echo get_stylesheet_directory_uri()."/img/Group 4.png";?>" alt="droit">
+            </div>
+        </article>
+        <article class="formrencontre">
+            <div class="encolonne">
+                <h2>Nous contacter</h2>
+                <div class="rencontrer">
+                    <div class="colonne"><label>Nom</label><input  type="text"></div>
+                    <div class="colonne"><label>E-mail</label><input  type="email"></div>
+                    <div class="colonne"><label>Message</label><textarea class="message" aria-errormessage=""></textarea></div>
+                    <button class="envoyer">Envoyer</button>
+                </div>
+            </div>
+            <img src="<?php echo get_stylesheet_directory_uri()."/img/Mask group(1).png";?>" alt="bas">
+        </article>
+        
+    </main>
+
 <?php require "footer.php"; ?>
