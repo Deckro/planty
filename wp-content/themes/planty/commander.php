8f2c361e6907b70bb3d92cb83408fd63e4f7664b
deckro <deck_soufly@hotmail.fr>
2023-07-03 09:47:50 +0200

    reboot

--- wp-content/themes/planty/commander.php
+++ wp-content/themes/planty/commander.php
@@ -1,50 +1,5 @@
 <?php /* Template Name: commander */ ?>
 <?php require "header.php"; ?>
-<main class="back">
-    <div class="taille">
-        <h1 class="commandertitre">COMMANDER</h1>
-    <article class="selection">
-        <h2 class="votre">votre commande</h2>
-        <div class="fruits">
-            <div>
-                <h3 class="fraise1">FRAISE</h3><img class="fruit1" src="<?php echo get_stylesheet_directory_uri()."/img/jeremy-bezanger-0QgbyZdhX7k-unsplash 1.png";?>" alt="fraise">
-            </div>
-            <div>
-		        <h3 class="pamplemousse1">PAMPLE </br>MOUSSE</h3><img class="fruit1" src="<?php echo get_stylesheet_directory_uri()."/img/estudio-bloom-tOitjphtIXU-unsplash 1.png";?>"  alt="pamplemousse">
-            </div>
-            <div>
-                <h3 class="framboise1">FRAMB </br>OISE</h3><img class="fruit1" src="<?php echo get_stylesheet_directory_uri()."/img/rodion-kutsaev-4k8xEFW4_3Q-unsplash 1.png";?>"  alt="framboise">
-            </div>
-            <div>
-		        <h3 class="citron1">CITRON</h3><img class="fruit1" src="<?php echo get_stylesheet_directory_uri()."/img/estudio-bloom-ezqnxsqUZ80-unsplash 1.png";?>"  alt="citron">
-            </div>
-        </div>
-        <div>
-            <input class="number" type="number" value="0" min="0" max="99999">
-            <button type="submit">OK</button>
-            <input class="number" type="number" value="0" min="0" max="99999">
-            <button type="submit">OK</button>
-            <input class="number" type="number" value="0" min="0" max="99999">
-            <button type="submit">OK</button>
-            <input class="number" type="number" value="0" min="0" max="99999">
-            <button type="submit">OK</button>
-        </div>
-    </article>
-    <article class="formulaire">
-        <div>
-            <h2>vos informations</h2>
-            <div class="colonne"><label>Nom</label><input type="text"></div>
-            <div class="colonne"><label>Prenom</label><input type="text"></div>
-            <div class="colonne"><label>E-mail</label><input type="email"></div>
-        </div>
-        <div>
-            <h2>Livraison</h2>
-            <div class="colonne"><label>Adresse de livraison</label><input type="text"></div>
-            <div class="colonne"><label>code postal</label><input type="text"></div>
-            <div class="colonne"><label>Ville</label><input type="text"></div>
-        </div>
-    </article>
-    <button type="submit">Commander</button>    
-    </div>
+<main>
 </main>
 <?php require "footer.php"; ?>
